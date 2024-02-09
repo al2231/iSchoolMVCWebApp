ea14992947059c92dd7bcbf9584389a581a61f11
al2231 <112445369+al2231@users.noreply.github.com>
2024-02-09 12:47:03 -0500

    views and root

---
+++ iSchoolWebApp/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    // change for home page, index to be About
+    pattern: "{controller=Home}/{action=About}/{id?}");
+
+app.Run();
---
+++ iSchoolWebApp/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(function () {
+    // make people (faculty / staff) into tabs
+    $('#peopleTabs').tabs();
+
+    // make employment tables into tabs
+    $('.tables').tabs();
+
+    $('#minorsAcc').accordion({
+        heightStyle: "content",
+        collapsible: true,
+        active: true
+    });
+    $('.coursesAcc').accordion({
+        heightStyle: "content",
+        collapsible: true,
+        active: true
+    });
+
+    new DataTable('#coopTable');
+    new DataTable('#profTable');
+
+    //turns everything on
+    $('#allDegrees').fadeIn(1000);
+    $('#minorsAcc').fadeIn(1000);
+    $('.tables').fadeIn(1000);
+    $('#allPeople').fadeIn(1000);
+
+});
